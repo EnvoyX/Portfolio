fa5450126956639761dcad640145f75da08560d3
EnvoyX <muhamadhanifhafizhan@gmail.com>
2024-01-13 13:07:33 +0700

    added contact

--- main.js
+++ main.js
@@ -13,3 +13,28 @@ function toggleDetail(event){
     const detail = $(item).children(".about-exp-item-header-detail")
     $(detail).slideToggle()
 }
+
+
+//Contact Validation
+function onFormSubmit(event){
+    event.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()){
+        alert("Email is required")
+    }
+    else if (!$(subject).val()){
+        alert("Subject is required")
+    }
+    else if (!$(message).val()){
+        alert("Message is required")
+    }
+    else{
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
